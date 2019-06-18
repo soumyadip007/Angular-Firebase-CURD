ca7a10109a0add105f7d8403a2d110330098130c
soumyadip007 <soumyadip.note@gmail.com>
2019-06-18 21:34:50 +0530

    AppComponent config (BUG)

--- firebase-demo/src/app/app.component.ts
+++ firebase-demo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase} from 'angularfire2/database';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firebase-demo';
+  courses:any[];
+
+  constructor(db: AngularFireDatabase){
+
+    db.list('/courses')
+    .subscribe(courses=>{
+      this.courses=courses;
+      console.log(this.courses);
+
+    });
+   }
 }
