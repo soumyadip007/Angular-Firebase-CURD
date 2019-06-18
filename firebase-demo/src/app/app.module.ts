fbaae92c38736dcdec085c506446b72183f1291e
soumyadip007 <soumyadip.note@gmail.com>
2019-06-18 20:59:56 +0530

    app module.conifg

--- firebase-demo/src/app/app.module.ts
+++ firebase-demo/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { environment } from 'src/environments/environment.prod';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment)
   ],
   providers: [],
   bootstrap: [AppComponent]
